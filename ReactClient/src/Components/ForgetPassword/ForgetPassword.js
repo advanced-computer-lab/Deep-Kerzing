96d4c977310de58336c48f91442973fd6f1fa330
George Sherif <georgesherif999@gmail.com>
2021-10-30 16:13:22 +0200

    NavagationManagement

---
+++ ReactClient/src/Components/ForgetPassword/ForgetPassword.js
@@ -0,0 +1,41 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "../searchFlight/adminSearchFlight.css";
+import axios from "axios";
+import { FaUser } from "react-icons/fa";
+
+const ForgetPassword = () => {
+  const [username, setUsername] = useState("");
+
+  const LoginHandler = (event) => {
+    event.preventDefault();
+    const input = {
+      username: username,
+    };
+    axios("", { input }).then((res) => {
+      console.log(res);
+      console.log(res.data);
+    });
+  };
+
+  return (
+    <div>
+      <form class="containerCard" onSubmit={LoginHandler}>
+        <div class="input-group input-group-icon">
+          <input
+            onChange={(event) => setUsername(event.target.value)}
+            type="text"
+            placeholder="Email"
+            required
+          />
+          <div class="input-icon">
+            <FaUser></FaUser>
+          </div>
+        </div>
+
+        <button className="button">Send!</button>
+      </form>
+    </div>
+  );
+};
+export default ForgetPassword;
---
+++ ReactClient/src/Pages/AdminFlights/AdminFlights.js
@@ -0,0 +1,7 @@
+import FlightDetails from "../../Components/FlightDetails/FlightDetails";
+const AdminFlights = () =>{
+    return (
+        <FlightDetails></FlightDetails>
+    )
+}
+export default AdminFlights;
